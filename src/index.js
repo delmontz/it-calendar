63ddfce1948313a8d3c86579884c96049f7631cb
delmont <pazxx219@gmail.com>
2018-04-02 19:00:45 +0900

    新規登録

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Daily Gadget Lab';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+  ],
+  devServer: {
+    contentBase: './public',
+  }
+};
