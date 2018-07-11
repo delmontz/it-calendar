6a2b99569929b20d1c409e3ebd1c9206f2a0cec7
delmontz <delmontz@dgl.tokyo>
2018-07-11 23:31:19 +0900

    DayPickerの日付選択をハイライト化

--- web/src/components/DayPickerComponent.jsx
+++ web/src/components/DayPickerComponent.jsx
@@ -1,68 +1,88 @@
 import React from 'react';
-import BaseDayPicker from 'react-day-picker';
+import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
 const MONTHS = [
-  '1月',
-  '2月',
-  '3月',
-  '4月',
-  '5月',
-  '6月',
-  '7月',
-  '8月',
-  '9月',
-  '10月',
-  '11月',
-  '12月'
-];
+   '1月',
+   '2月',
+   '3月',
+   '4月',
+   '5月',
+   '6月',
+   '7月',
+   '8月',
+   '9月',
+   '10月',
+   '11月',
+   '12月'
+ ];
+ 
+ const WEEKDAYS_LONG = [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日'
+ ];
+ 
+ const WEEKDAYS_SHORT = ['日', '月', '火', '水', '木', '金', '土'];
+ 
+ const modifiers = {
+   sunday: { daysOfWeek: [0] }, 
+   saturday: { daysOfWeek: [6] } // daysOfWeek: [0, 6]で土日を範囲指定
+ };
+ 
+ const modifiersStyles = {
+   sunday: {
+     color: '#ff4500',
+     backgroundColor: '#fffdee'
+   },
+   saturday: {
+     color: '#4169e1',
+     backgroundColor: '#fffdee'
+   },
+ };
 
-const WEEKDAYS_LONG = [
-   '日曜日',
-   '月曜日',
-   '火曜日',
-   '水曜日',
-   '木曜日',
-   '金曜日',
-   '土曜日'
-];
+ const toDayStyle = `.DayPicker-Day--today {
+   background-color: #2f4f4f;
+   color: white;
+ }`;
 
-const WEEKDAYS_SHORT = ['日', '月', '火', '水', '木', '金', '土'];
-
-const modifiers = {
-  sunday: { daysOfWeek: [0] }, 
-  saturday: { daysOfWeek: [6] } // daysOfWeek: [0, 6]で土日を範囲指定
-};
-
-const modifiersStyles = {
-  sunday: {
-    color: '#ff4500',
-    backgroundColor: '#fffdee'
-  },
-  saturday: {
-    color: '#4169e1',
-    backgroundColor: '#fffdee'
-  },
-};
-
-const toDayStyle = `.DayPicker-Day--today {
-  background-color: #2f4f4f;
-  color: white;
-}`;
-
-export default function DayPicker() {
-  return(
-    <div>
-      <style>{toDayStyle}</style>
-      <BaseDayPicker 
-        locale="ja"
-        months={MONTHS}
-        weekdaysLong={WEEKDAYS_LONG}
-        weekdaysShort={WEEKDAYS_SHORT}
-        firstDayOfWeek={0} //日曜日始まり
-        modifiers={modifiers}
-        modifiersStyles={modifiersStyles}
-      />
-    </div>
-  );
+export default class DayPickerComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.state = {
+      selectedDay: null,
+    };
+  }
+  handleDayClick(day, { selected }) {
+    this.setState({
+      selectedDay: selected ? undefined : day,
+    });
+  }
+  render() {
+    return (
+      <div>
+          <p>
+          {this.state.selectedDay
+            ? ('選択中の日付　' + this.state.selectedDay.toLocaleDateString())
+            : '>>日付を選択してください<< 👻'}
+        </p>
+        <DayPicker
+         locale="ja"
+         months={MONTHS}
+         weekdaysLong={WEEKDAYS_LONG}
+         weekdaysShort={WEEKDAYS_SHORT}
+         firstDayOfWeek={0} //日曜日始まり
+         modifiers={modifiers}
+         modifiersStyles={modifiersStyles}
+         selectedDays={this.state.selectedDay}
+         onDayClick={this.handleDayClick}
+        />
+      </div>
+    );
+  }
 }
