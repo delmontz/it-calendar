271d44965821e2ea970e8640f721caba5e7f76fa
delmontz <delmontz@dgl.tokyo>
2018-10-16 03:47:16 +0900

    ドキュメントのフォーマット

--- tmp/init_data_v2.js
+++ tmp/init_data_v2.js
@@ -18,29 +18,29 @@ firebase.initializeApp({
    projectId: ENV.FB_PROJECT_ID
 });
 const db = firebase.firestore();
-db.settings({timestampsInSnapshots: true});
+db.settings({ timestampsInSnapshots: true });
 
 /* conpass-apiよりデータ取得 */
 createMonthlyEventTbl(PERIOD).then(() => {
-  console.log('完了');
+   console.log('完了');
 });
 
 /****************************************/
 
-async function createMonthlyEventTbl(period){
-   let year = period.slice(0,4);
+async function createMonthlyEventTbl(period) {
+   let year = period.slice(0, 4);
    let month = period.slice(4);
    let acquired_data;
    let event_id_record = [];
 
    /* 日毎にデータ書き込み */
-   for(let day = 1; day <= getDay(period); day++){
-		let _acquired_num = 0;
-		acquired_data = {available: 1}; /* 必ず一度は実行 */
-      for(let n = 0; acquired_data.available > 0; n++){
-        acquired_data = await getDailyEventData(year, month, day, ((n * ACQUISITION) + 1));
-        Array.prototype.push.apply(event_id_record, acquired_data.event_id_tbl);
-        _acquired_num++;
+   for (let day = 1; day <= getDay(period); day++) {
+      let _acquired_num = 0;
+      acquired_data = { available: 1 }; /* 必ず一度は実行 */
+      for (let n = 0; acquired_data.available > 0; n++) {
+         acquired_data = await getDailyEventData(year, month, day, ((n * ACQUISITION) + 1));
+         Array.prototype.push.apply(event_id_record, acquired_data.event_id_tbl);
+         _acquired_num++;
       }
       console.log('ステージ:' + _acquired_num);
       console.log('###################################');
@@ -51,56 +51,56 @@ async function createMonthlyEventTbl(period){
    });
    /* イベントIDと更新時間テーブルの書き込み */
    db.collection('EventData').doc('conpass').collection(PERIOD).doc('0_event_tbl')
-      .set({'event_id_tbl': event_id_record})
+      .set({ 'event_id_tbl': event_id_record })
       .then(() => {
          console.log('イベントIDテーブルの書き込み完了');
       }).catch(err => {
          console.log(err);
-   });
+      });
 }
 
-async function getDailyEventData(year, month, day, index){
+async function getDailyEventData(year, month, day, index) {
    let eventTbl = {
       available: 0,
       event_data: [],
       event_id_tbl: []
    };
    let acquired_data = {};
-   try{
+   try {
       acquired_data = await web.get('https://connpass.com/api/v1/event/')
-     .query({
-        ymd: year + month + day,
-        count: ACQUISITION,
-        order: 3,
-        start: index
-     });
-     console.log(day + '日目取得完了');
-     console.log('取得数:' + acquired_data.body.results_returned);
-  }catch(err){
-     console.log(err);
-  }
+         .query({
+            ymd: year + month + day,
+            count: ACQUISITION,
+            order: 3,
+            start: index
+         });
+      console.log(day + '日目取得完了');
+      console.log('取得数:' + acquired_data.body.results_returned);
+   } catch (err) {
+      console.log(err);
+   }
 
-  /* 取得データより構造体作成 */
-  let acquired_body = acquired_data.body; 
-  for(let i = 0; i < acquired_body.results_returned; i++){
-     let events = acquired_body.events[i];
-     let eventContent = {
-        event_id: events.event_id,
-        title: events.title,
-        description: events.description,
-        open_time: events.started_at,
-        place: events.place,
-        address: events.address,
-        url: events.event_url,
-        updated: events.updated_at
-     };
-     eventTbl.event_data.push(eventContent);
-  }
-  eventTbl.available = acquired_body.results_available - (index + ACQUISITION);
+   /* 取得データより構造体作成 */
+   let acquired_body = acquired_data.body;
+   for (let i = 0; i < acquired_body.results_returned; i++) {
+      let events = acquired_body.events[i];
+      let eventContent = {
+         event_id: events.event_id,
+         title: events.title,
+         description: events.description,
+         open_time: events.started_at,
+         place: events.place,
+         address: events.address,
+         url: events.event_url,
+         updated: events.updated_at
+      };
+      eventTbl.event_data.push(eventContent);
+   }
+   eventTbl.available = acquired_body.results_available - (index + ACQUISITION);
 
    /* レコードにセット書き込み,イベントIDテーブルの作成*/
    let batch = db.batch();
-   for(let i = 0; i < eventTbl.event_data.length; i++){
+   for (let i = 0; i < eventTbl.event_data.length; i++) {
       let event_id = eventTbl.event_data[i].event_id;
       let record = db.collection('EventData').doc('conpass').collection(PERIOD).doc(String(event_id));
       batch.set(record, eventTbl.event_data[i]);
@@ -115,8 +115,8 @@ async function getDailyEventData(year, month, day, index){
    return eventTbl;
 }
 
-function getDay(period){
-   return(new Date(period.slice(0,4), period.slice(4), 0).getDate());
+function getDay(period) {
+   return (new Date(period.slice(0, 4), period.slice(4), 0).getDate());
 }
 
 
--- tmp/update_data_v1.js
+++ tmp/update_data_v1.js
@@ -10,7 +10,7 @@ const ACQUISITION = 100;
 
 /* conpassAPI config */
 const UPDATETIME = 1;
-const	OPENTDATE = 2;
+const OPENTDATE = 2;
 const NEWER = 3;
 
 /**************************************************************************************/
@@ -23,7 +23,7 @@ firebase.initializeApp({
    projectId: ENV.FB_PROJECT_ID
 });
 const db = firebase.firestore();
-db.settings({timestampsInSnapshots: true});
+db.settings({ timestampsInSnapshots: true });
 
 getEventTbl(PERIOD).then(event_tbl => {
    /* 新着確認 */
@@ -32,94 +32,94 @@ getEventTbl(PERIOD).then(event_tbl => {
 
 /**************************************************************************************/
 
-async function getConpassMasterTbl(period, index, order){
+async function getConpassMasterTbl(period, index, order) {
    let eventdata = {
       available: 0,
       event_data_tbl: []
    };
    let acquired_data = {};
-   try{
+   try {
       acquired_data = await web.get('https://connpass.com/api/v1/event/')
-     .query({
-        ym: period,
-        count: ACQUISITION,
-        order: order,
-        start: index
-     });
-  }catch(err){
-     console.log(err);
-  }
-  console.log('受信完了');
-  /* 取得データより構造体作成 */
-  let acquired_body = acquired_data.body; 
-  for(let i = 0; i < acquired_body.results_returned; i++){
-     let events = acquired_body.events[i];
-     let eventContent = {
-        event_id: events.event_id,
-        title: events.title,
-        description: events.description,
-        open_time: events.started_at,
-        place: events.place,
-        address: events.address,
-        url: events.event_url,
-        updated: events.updated_at
-     };
-     eventdata.event_data_tbl.push(eventContent);
-  }
-  eventdata.available = acquired_body.results_available - (index + ACQUISITION);
+         .query({
+            ym: period,
+            count: ACQUISITION,
+            order: order,
+            start: index
+         });
+   } catch (err) {
+      console.log(err);
+   }
+   console.log('受信完了');
+   /* 取得データより構造体作成 */
+   let acquired_body = acquired_data.body;
+   for (let i = 0; i < acquired_body.results_returned; i++) {
+      let events = acquired_body.events[i];
+      let eventContent = {
+         event_id: events.event_id,
+         title: events.title,
+         description: events.description,
+         open_time: events.started_at,
+         place: events.place,
+         address: events.address,
+         url: events.event_url,
+         updated: events.updated_at
+      };
+      eventdata.event_data_tbl.push(eventContent);
+   }
+   eventdata.available = acquired_body.results_available - (index + ACQUISITION);
 
-  return eventdata;
+   return eventdata;
 }
 
-async function getEventTbl(period){
+async function getEventTbl(period) {
    let event_tbl;
    let record_ref = db.collection('EventData').doc('conpass').collection(period).doc('0_event_tbl');
    await record_ref.get().then(snapshot => {
       event_tbl = snapshot.data();
-	}).catch(err => {
-			console.log(err);
+   }).catch(err => {
+      console.log(err);
    });
    return event_tbl;
 }
 
-async function checkNewer(event_tbl){
-	let new_event_data_tbl = [];
+async function checkNewer(event_tbl) {
+   let new_event_data_tbl = [];
    let new_event_id_tbl = [];
    let update_flg = false;
    let next_page_flg = true;
    let event_id_tbl = event_tbl.event_id_tbl;
-	for(let n = 0; next_page_flg; n++){
-		console.log('%d順目', n);
-		await getConpassMasterTbl(PERIOD, ((n * ACQUISITION) + 1), NEWER).then(conpass_tbl => {
-			conpass_tbl.event_data_tbl.forEach((eventdata, index) => {
-				next_page_flg = false;
-				if(!event_id_tbl.includes(eventdata.event_id)){
+   for (let n = 0; next_page_flg; n++) {
+      console.log('%d順目', n);
+      await getConpassMasterTbl(PERIOD, ((n * ACQUISITION) + 1), NEWER).then(conpass_tbl => {
+         conpass_tbl.event_data_tbl.forEach((eventdata, index) => {
+            next_page_flg = false;
+            if (!event_id_tbl.includes(eventdata.event_id)) {
                console.log('イベントID:%dを追加', eventdata.event_id);
                new_event_id_tbl.push(eventdata.event_id);
-					new_event_data_tbl.push(eventdata);
-					update_flg = true;
-					/* 最終インデックスが更新対象だった場合次のループも実行 */
-					if(((index + 1) % ACQUISITION) == 0){
-						next_page_flg = true;
-					}
-				}
-			});
-		});
-	}
-	/* 新着データ登録 */
-	if(update_flg){
+               new_event_data_tbl.push(eventdata);
+               update_flg = true;
+               /* 最終インデックスが更新対象だった場合次のループも実行 */
+               if (((index + 1) % ACQUISITION) == 0) {
+                  next_page_flg = true;
+               }
+            }
+         });
+      });
+   }
+   /* 新着データ登録 */
+   if (update_flg) {
       /* イベントID＆更新時間テーブル更新 */
       Array.prototype.push.apply(event_id_tbl, new_event_id_tbl);
       db.collection('EventData').doc('conpass').collection(PERIOD).doc('0_event_tbl')
-         .set({'event_id_tbl': event_id_tbl})
+         .set({ 'event_id_tbl': event_id_tbl })
          .then(() => {
             console.log('イベントIDテーブルの書き込み完了');
-      }).catch(err => {
-         console.log(err);
-      });
+         }).catch(err => {
+            console.log(err);
+         });
       /* イベントデータテーブル更新 */
       let batch = db.batch();
-      for(let i = 0; i < new_event_data_tbl.length; i++){
+      for (let i = 0; i < new_event_data_tbl.length; i++) {
          let event_id = new_event_data_tbl[i].event_id;
          let record = db.collection('EventData').doc('conpass').collection(PERIOD).doc(String(event_id));
          batch.set(record, new_event_data_tbl[i]);
@@ -129,6 +129,27 @@ async function checkNewer(event_tbl){
       }).catch(err => {
          console.log(err);
       });
-	}
-	console.log('%s:新着チェック完了', PERIOD);
+   }
+   console.log('%s:新着チェック完了', PERIOD);
+}
+
+async function checkUpdate() {
+   for (let n = 0; next_page_flg; n++) {
+      console.log('%d順目', n);
+      await getConpassMasterTbl(PERIOD, ((n * ACQUISITION) + 1), NEWER).then(conpass_tbl => {
+         conpass_tbl.event_data_tbl.forEach((eventdata, index) => {
+            next_page_flg = false;
+            if (!event_id_tbl.includes(eventdata.event_id)) {
+               console.log('イベントID:%dを追加', eventdata.event_id);
+               new_event_id_tbl.push(eventdata.event_id);
+               new_event_data_tbl.push(eventdata);
+               update_flg = true;
+               /* 最終インデックスが更新対象だった場合次のループも実行 */
+               if (((index + 1) % ACQUISITION) == 0) {
+                  next_page_flg = true;
+               }
+            }
+         });
+      });
+   }
 }
