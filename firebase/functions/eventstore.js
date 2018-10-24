3dc428c3e5146d6973f6274a9565a5ff70dee08a
delmontz <delmontz@dgl.tokyo>
2018-10-24 15:26:35 +0900

    getEventDataのCORS対策

--- firebase/functions/eventstore.js
+++ firebase/functions/eventstore.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const firebase = require('firebase');
+const cors = require('cors')({origin: true});
 require('firebase/firestore');
 
 // // Create and Deploy Your First Cloud Functions
@@ -14,15 +15,18 @@ firebase.initializeApp({
 const DB = firebase.firestore();
 DB.settings({ timestampsInSnapshots: true });
 
-exports.getEventData = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
-   let ym = req.query.ymd.substr(0, 6);
-   let day = Number(req.query.ymd.substr(6, 2));
-   let event_data_tbl = [];
-   let ref = DB.collection('EventData').doc('conpass').collection(ym).where('date', '==', day);
-   await ref.get().then(snapshot => {
-      snapshot.forEach(event_data => {
-         event_data_tbl.push(event_data.data());
+/* param: ymd */
+exports.getEventData = functions.region('asia-northeast1').https.onRequest((req, res) => {
+   cors(req, res, async () => {
+      let ym = req.query.ymd.substr(0, 6);
+      let day = Number(req.query.ymd.substr(6, 2));
+      let event_data_tbl = [];
+      let ref = DB.collection('EventData').doc('conpass').collection(ym).where('date', '==', day);
+      await ref.get().then(snapshot => {
+         snapshot.forEach(event_data => {
+            event_data_tbl.push(event_data.data());
+         });
       });
+      res.status(200).send(event_data_tbl);
    });
-   res.status(200).send(event_data_tbl);
 });
