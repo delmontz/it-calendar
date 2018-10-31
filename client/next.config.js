4b62f67a29e55cfcd01f2df77211c86eeef16adf
delmontz <delmontz@dgl.tokyo>
2018-10-31 23:00:50 +0900

    ファイル整理

--- client/next.config.js
+++
@@ -1,8 +0,0 @@
-const withCSS = require('@zeit/next-css');
-const exportPathMap =  async function (defaultPathMap) {
-   return {
-     '/': { page: '/' },
-   }
-}
-
-module.exports = withCSS(exportPathMap());
