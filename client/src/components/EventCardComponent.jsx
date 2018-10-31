9c928133393f56fddadce030183fb1b8c37f4a0e
delmontz <delmontz@dgl.tokyo>
2018-10-31 23:12:50 +0900

    カードコンテンツの文字スタイル変更

--- client/src/components/EventCardComponent.jsx
+++ client/src/components/EventCardComponent.jsx
@@ -40,12 +40,12 @@ class EventCardComponent extends React.Component {
                      title={<h1>{eventData.title}</h1>}
                   />
                   <Typography variant="h6">📅日時</Typography>
-                  <Typography variant="body1" color="textSecondary">{this.getOpenTime(eventData.open_time)}</Typography>
+                  <Typography variant="body2">{this.getOpenTime(eventData.open_time)}</Typography>
                   <Typography variant="h6">🌏場所</Typography>
-                  <Typography variant="body1" color="textSecondary">{eventData.place}</Typography>
+                  <Typography variant="body2">{eventData.place}</Typography>
                   <Typography variant="h6">🏡住所</Typography>
-                  <Typography variant="body1" color="textSecondary">{eventData.address}</Typography>
-                  <Typography variant="body1" color="textSecondary"><div dangerouslySetInnerHTML={{ __html: eventData.description }} /></Typography>
+                  <Typography variant="body2">{eventData.address}</Typography>
+                  <Typography variant="body2"><div dangerouslySetInnerHTML={{ __html: eventData.description }} /></Typography>
                </CardContent>
             </Card>
          </div>
